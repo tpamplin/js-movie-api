88f04370a72b6d341cef0db8f2f6c6f64c335d61
tpamplin <gartiman1@gmail.com>
2024-07-22 09:14:45 -0400

    add auto routing to documentation

--- src/js/server.js
+++ src/js/server.js
@@ -8,7 +8,20 @@ http.createServer((request, response) => {
         filePath = "";
 
     if (q.pathname.includes("documentation")) {
+        filePath = __dirname + "/documentation.html";
+    } else {
+        filePath = "index.html";
     }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(data);
+        response.end();
+    });
 }).listen(8080);
 
 console.log("My first Node test server ios running on port 8080.");
