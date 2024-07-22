d2b967c6a75eb5234ad3e0296a114259b40ce2c9
tpamplin <gartiman1@gmail.com>
2024-07-22 08:46:05 -0400

    create documentation.html

--- src/js/server.js
+++ src/js/server.js
@@ -1,8 +1,14 @@
-const http = require("http");
+const http = require("http"),
+    fs = require("fs"),
+    url = require("url");
 
 http.createServer((request, response) => {
-    response.writeHead(200, { "content-type": "text/plain" });
-    response.end("Hello, Node!\n");
+    let addr = request.url,
+        q = new URL(addr, "http://localhost:8080"),
+        filePath = "";
+
+    if (q.pathname.includes("documentation")) {
+    }
 }).listen(8080);
 
 console.log("My first Node test server ios running on port 8080.");
