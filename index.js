9c6341e57a5f86e2e4273ed66452a20e191e2e31
tpamplin <gartiman1@gmail.com>
2024-07-24 08:46:29 -0400

    add server responses

--- index.js
+++ index.js
@@ -2,5 +2,24 @@
 // Movie API responds to requests for information about movies and users
 
 // import Express
-const express = require("express");
+const express = require("express"),
+    morgan = require("morgan");
+
 const app = express();
+
+// Logging
+app.use(morgan("common"));
+
+// Server Responses
+app.get("/", (req, res) => {
+    res.send("Welcome to my app!");
+});
+
+app.get("/documentation", (req, res) => {
+    res.sendFile("public/documentation.html", { root: __dirname });
+});
+
+//deploy server
+app.listen(8080, () => {
+    console.log("Your app is listening on port 8080.");
+});
