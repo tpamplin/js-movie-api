cbe69a2c9b4d8e669ef5754932b4469b55735af1
tpamplin <gartiman1@gmail.com>
2024-07-24 09:49:27 -0400

    added top-movies page

--- index.js
+++ index.js
@@ -12,6 +12,52 @@ const app = express();
 const accessLogStream = fs.createwritestream(path.join(__dirname, "log.txt"), {
     flags: "a",
 });
+
+//json movies object
+
+let topMovies = [
+    {
+        title: "The Princess Bride",
+        year: "1987",
+    },
+    {
+        title: "The Matrix",
+        year: "1999",
+    },
+    {
+        title: "Ip Man",
+        year: "2008",
+    },
+    {
+        title: "Pulp Fiction",
+        year: "1994",
+    },
+    {
+        title: "The Dark Knight",
+        year: "2008",
+    },
+    {
+        title: "Interstellar",
+        year: "2014",
+    },
+    {
+        title: "Shooter",
+        year: "2007",
+    },
+    {
+        title: "Taken",
+        year: "2008",
+    },
+    {
+        title: "Shaun of the Dead",
+        year: "2004",
+    },
+    {
+        title: "Spider-Man",
+        year: "2002",
+    },
+];
+
 // Logging
 app.use(morgan("combined", { stream: accessLogStream }));
 
@@ -20,8 +66,15 @@ app.get("/", (req, res) => {
     res.send("Welcome to my app!");
 });
 
-app.get("/documentation", (req, res) => {
-    res.sendFile("public/documentation.html", { root: __dirname });
+app.get("/top-movies", (req, res) => {
+    res.json(topMovies);
+});
+
+app.use(express.static("public"));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("A wild Error has appeared!");
 });
 
 //deploy server
