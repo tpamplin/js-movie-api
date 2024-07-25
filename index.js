c1e2e008f138e5e507074140dfb73d3aef19b0f2
tpamplin <gartiman1@gmail.com>
2024-07-25 08:25:17 -0400

    Move topMovies into an external file.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Timothy Pamplin 2024
 // Movie API responds to requests for information about movies and users
 
-// import Express
+// import dependencies
 const express = require("express"),
     morgan = require("morgan"),
     fs = require("fs"),
@@ -9,55 +9,11 @@ const express = require("express"),
 
 const app = express();
 
-const accessLogStream = fs.createwritestream(path.join(__dirname, "log.txt"), {
+// Logging
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
     flags: "a",
 });
 
-//json movies object
-let topMovies = [
-    {
-        title: "The Princess Bride",
-        year: "1987",
-    },
-    {
-        title: "The Matrix",
-        year: "1999",
-    },
-    {
-        title: "Ip Man",
-        year: "2008",
-    },
-    {
-        title: "Pulp Fiction",
-        year: "1994",
-    },
-    {
-        title: "The Dark Knight",
-        year: "2008",
-    },
-    {
-        title: "Interstellar",
-        year: "2014",
-    },
-    {
-        title: "Shooter",
-        year: "2007",
-    },
-    {
-        title: "Taken",
-        year: "2008",
-    },
-    {
-        title: "Shaun of the Dead",
-        year: "2004",
-    },
-    {
-        title: "Spider-Man",
-        year: "2002",
-    },
-];
-
-// Logging
 app.use(morgan("combined", { stream: accessLogStream }));
 
 // Server Responses
@@ -66,17 +22,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
+    //Reading topMovies from an external json file.
+    var topMovies;
+    fs.readFile("./public/json/topMovies.json", "utf8", (err, data) => {
+        if (err) throw err;
+        topMovies = JSON.parse(data);
+    });
+
     res.json(topMovies);
 });
 
 app.use(express.static("public"));
 
+// Error handling.
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send("A wild Error has appeared!");
 });
 
-//deploy server
+// deploy server
 app.listen(8080, () => {
     console.log("Your app is listening on port 8080.");
 });
