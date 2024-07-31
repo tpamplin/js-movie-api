2251e32ef1e381a36870de351ec72d9e11b57af3
tpamplin <gartiman1@gmail.com>
2024-07-31 08:52:45 -0400

    added temporary endpoints to index.js

--- index.js
+++ index.js
@@ -20,14 +20,62 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
 app.use(morgan("combined", { stream: accessLogStream }));
 
 // Server Responses
+//default
 app.get("/", (req, res) => {
-    res.send("Welcome to my app!");
+    res.send("Welcome to my api!");
 });
 
+//Returns a JSON object containing a list of all movies.
 app.get("/movies", (req, res) => {
     //Reading topMovies from an external json file.
-    var topMovies = require("./public/json/topMovies.json");
-    res.json(topMovies);
+    var movies = require("./public/json/movies.json");
+    res.json(movies);
+});
+
+//Returns a JSON object containing all information about a specific movie.
+app.get("/movies/:id", (req, res) => {
+    res.send("Successful GET request returning data on a specific movie.");
+});
+
+//Returns a JSON object containing data about a specific genre.
+app.get("/genres/:name", (req, res) => {
+    res.send("Successful GET request, returning data on a specific genre.");
+});
+
+//Returns a JSON object containing information about a specific director.
+app.get("/directors/:name", (req, res) => {
+    res.send("Successful GET request, returning data on a specific director.");
+});
+
+//Adds a new user to the list of users.
+app.post("/users", (req, res) => {
+    res.send("Successful POST request, adding a user to the list of users.");
+});
+
+//Updates the username to the new username the user has picked.
+app.put("/users/:id/username", (req, res) => {
+    res.send("Successful PUT request, updating a username.");
+});
+
+//Adds a movie to a user's list of favorite movies.
+app.post("/users/:id/favorites", (req, res) => {
+    res.send(
+        "Successful POST request, adding a movie to a user's favorites list."
+    );
+});
+
+//Removes a movie from a user's favorite list.
+app.delete("/users/:id/favorites/:id", (req, res) => {
+    res.send(
+        "Successful DELETE request, removing a movie from user's favorites list."
+    );
+});
+
+//Removes a user from the list of users.
+app.delete("/users/:id", (req, res) => {
+    res.send(
+        "Sucessful DELETE request, removing a user from the list of users."
+    );
 });
 
 app.use(express.static("public"));
