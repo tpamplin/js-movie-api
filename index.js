701616c451d34ce014a4c6b08f19e4ee710e7f60
tpamplin <gartiman1@gmail.com>
2024-09-18 17:49:06 -0400

    Fix cors to allow access from all origins

--- index.js
+++ index.js
@@ -27,20 +27,22 @@ app.use(bodyParser.json());
 
 const cors = require("cors");
 
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+app.use(cors());
 
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-                return callback(new Error(message), false);
-            }
-            return callback(null, true);
-        },
-    })
-);
+// let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+
+// app.use(
+//     cors({
+//         origin: (origin, callback) => {
+//             if (!origin) return callback(null, true);
+//             if (allowedOrigins.indexOf(origin) === -1) {
+//                 let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+//                 return callback(new Error(message), false);
+//             }
+//             return callback(null, true);
+//         },
+//     })
+// );
 
 let auth = require("./routes/auth")(app);
 
