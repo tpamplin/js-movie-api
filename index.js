14fee62745516aff7c40d18d0a4227c34f7586f4
tpamplin <gartiman1@gmail.com>
2024-11-25 10:59:02 -0500

    specifically allow localhost:1234

--- index.js
+++ index.js
@@ -29,20 +29,20 @@ const cors = require("cors");
 
 app.use(cors());
 
-// let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+let allowedOrigins = ["http://localhost:1234"];
 
-// app.use(
-//     cors({
-//         origin: (origin, callback) => {
-//             if (!origin) return callback(null, true);
-//             if (allowedOrigins.indexOf(origin) === -1) {
-//                 let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-//                 return callback(new Error(message), false);
-//             }
-//             return callback(null, true);
-//         },
-//     })
-// );
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+                return callback(new Error(message), false);
+            }
+            return callback(null, true);
+        },
+    })
+);
 
 let auth = require("./routes/auth")(app);
 
