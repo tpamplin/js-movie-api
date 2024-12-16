4d80c41e83e205761dbfc7ffb9429fd1fcd9b90f
tpamplin <gartiman1@gmail.com>
2024-12-16 11:07:20 -0500

    add unifiedtopology and newUrlParser

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ app.use(express.json());
 const mongoose = require("mongoose");
 
 // mongoose.connect("mongodb://localhost:27017/myFlixDB", { useNewUrlParser: true, useUnifiedTopology: true });
-
-mongoose.connect(process.env.CONNECTION_URI);
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Logging
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
